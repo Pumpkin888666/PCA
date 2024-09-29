47eef50d41d242cc91b7600b4a7ade063fb58f84
Pumpkin888666 <aswdfgyhj@163.com>
2024-09-29 11:11:08 +0800

    修改了数据文件

--- data/database/config.php
+++ data/database/config.php
@@ -1,9 +1,9 @@
 <?php
 $dbconfig = array(
     'host' => 'localhost',
-    'dbname' => 'api_pumpkin8_cn', // 数据库名
+    'dbname' => '', // 数据库名
     'port' => 3306,
-    'user' => 'api_pumpkin8_cn', // 用户名
-    'pwd' => '8S7N6xGXbPEcnDy3', // 密码
+    'user' => '', // 用户名
+    'pwd' => '', // 密码
 );
 ?>
